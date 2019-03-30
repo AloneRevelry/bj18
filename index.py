b219e106763198f40daebd006d355ccbc88237a1
AloneRevelry <610316568@qq.com>
2019-03-30 23:10:01 +0800

    update index.py

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
 print('hello,github!')
+from django.shortcuts import redirect 
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('index')
+
+def login(request):
+	return redirect('/index')
